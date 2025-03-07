3841ef557382ff592ce609903d3d3058f1948716
Incognitobotz <177184463+Incognitobotz@users.noreply.github.com>
2025-03-07 19:16:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 USERNAME = environ.get('USERNAME', "https://t.me/BarkesyaXboy") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002198026835'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+WnJamUqcp_UwNDM1')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001931651573').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002171030525').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://INCOGNITOBOTS:INCOGNITOBOTS@cluster0.wdovemr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
